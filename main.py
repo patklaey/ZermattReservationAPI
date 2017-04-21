5543a4d0cf3f6988f8f85cb8026e34b54aa95a23
Pat Kläy <pat.klaey@swisscom.com>
2017-04-21 12:18:32 +0200

    Handling basic auth to not send WWW-Authenticate header in response if wrong password submitted

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, g
+from flask import Flask, jsonify, g, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, set_access_cookies, jwt_required, unset_jwt_cookies
 
 app = Flask(__name__)
@@ -9,7 +8,6 @@ app.config.from_pyfile('./config/config.py')
 db = SQLAlchemy(app)
 CORS(app, supports_credentials=True)
 
-basic_auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 from DB.User import User
@@ -21,15 +19,22 @@ def index():
 
 
 @app.route('/token')
-@basic_auth.login_required
 def get_auth_token():
+    if not request.authorization:
+        response = make_response(jsonify({'error':'Login required'}))
+        response.headers.set('WWW-Authenticate', 'Basic realm="patklaey.ch"')
+        return response, 401
+
+    if not verify_password(request.authorization.username, request.authorization.password):
+        response = jsonify({'error':'Invalid username or password'})
+        return response, 401
+
     token = g.user.generate_auth_token()
     response = jsonify({'token': token})
     set_access_cookies(response, token)
     return response, 200
 
 
-@basic_auth.verify_password
 def verify_password(username, password):
     user = User.query.filter_by(username=username, active=True).first()
     if not user or not user.verify_password(password):
